992c1f90b550c76a0d843ff0da33d7338d7d3383
shahinas327 <112380003+shahinas327@users.noreply.github.com>
2022-12-06 01:00:01 +0530

    StudentappnwBackendApplication

---
+++ src/main/java/com/example/studentappnw_backend/StudentappnwBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentappnw_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentappnwBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentappnwBackendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/studentappnw_backend/controller/StudentController.java
@@ -0,0 +1,13 @@
+package com.example.studentappnw_backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+
+    @GetMapping("/")
+    public String Homepage(){
+        return "welcpome to my website";
+    }
+}
